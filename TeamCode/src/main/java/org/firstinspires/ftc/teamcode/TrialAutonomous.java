0488c6bde90060e67a790fddef9b5c3a1791772d
Aayush <ambase6@gmail.com>
2019-10-17 18:51:37 -0500

    First autonomous code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrialAutonomous.java
@@ -0,0 +1,97 @@
+package org.firstinspires.ftc.teamcode;/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="trialAutonomous", group="Linear Opmode")
+//@Disabled
+public class TrialAutonomous extends LinearOpMode{
+
+    private DcMotor frontRight = null;
+    private DcMotor backRight = null;
+    private DcMotor frontLeft = null;
+    private DcMotor backLeft = null;
+
+    // Declare OpMode members.
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        frontRight  = hardwareMap.dcMotor.get("Frontright");
+        backRight = hardwareMap.dcMotor.get("Backright");
+        frontLeft= hardwareMap.dcMotor.get("Frontleft");
+        backLeft= hardwareMap.dcMotor.get("Backleft");
+
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+
+
+
+        waitForStart();
+
+        frontRight.setPower(1);
+        backRight.setPower(1);
+        frontLeft.setPower(1);
+        backLeft.setPower(1);
+        sleep(800);
+        //go forward
+
+        frontRight.setPower(1);
+        backRight.setPower(1);
+        frontLeft.setPower(-1);
+        backLeft.setPower(-1);
+        sleep(700);
+        //turn right
+
+        frontRight.setPower(1);
+        backRight.setPower(1);
+        frontLeft.setPower(1);
+        backLeft.setPower(1);
+        sleep(700);
+        //go forward
+    }
+}
