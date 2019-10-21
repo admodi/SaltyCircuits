56190f3f2ba1af8cbeb2936393dcd1be90c88d98
Aayush <ambase6@gmail.com>
2019-10-20 20:06:12 -0500

    Drivetrain Finial 10/14/2019

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrialAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrialAutonomous.java
@@ -55,7 +55,7 @@ public class  TrialAutonomous extends LinearOpMode{
     private DcMotor frontLeft = null;
     private DcMotor backLeft = null;
 
-    // Declare OpMode Members.
+    // Declare OpMode members.
 
     @Override
     public void runOpMode() throws InterruptedException {
